ab01a8f2e02ab6911e692f3b42e5e3ed57331d39
Stan Carver II <stan@a1webconsulting.com>
2026-09-02 21:19:33 -0500

    Load engine contribution registry

--- lib/active_admin/react.rb
+++ lib/active_admin/react.rb
@@ -4,6 +4,8 @@ require 'active_admin'
 require_relative 'react/arbre'
 require_relative 'react/mount'
 require_relative 'react/version'
+require_relative 'react/registry'
+require_relative 'react/contributions'
 
 module ActiveAdmin
   module React
