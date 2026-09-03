8b5bbc4b9aed81829409702d10fba4b20c2b19ab
Stan Carver II <stan@a1webconsulting.com>
2026-09-02 21:17:46 -0500

    Load Arbre mount integration

--- lib/active_admin/react.rb
+++ lib/active_admin/react.rb
@@ -2,9 +2,13 @@
 
 require 'active_admin'
 require_relative 'react/version'
+require_relative 'react/mount'
+require_relative 'react/arbre'
 
 module ActiveAdmin
   module React
     class Error < StandardError; end
   end
 end
+
+Arbre::Element.include(ActiveAdmin::React::Arbre)
