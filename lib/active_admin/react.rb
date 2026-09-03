7c92bfb0ef4a99ef75b64404c799dba1d3d3ac12
Stan Carver II <stan@a1webconsulting.com>
2026-09-02 20:39:48 -0500

    fix: resolve remaining RuboCop offenses

--- lib/active_admin/react.rb
+++ lib/active_admin/react.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
-require "active_admin"
-require_relative "react/version"
+require 'active_admin'
+require_relative 'react/version'
 
 module ActiveAdmin
   module React
